e002fbff3e8bcfa9eb1e5d5d7ad52d4d9e4527a5
Byron Sha <byronysha@gmail.com>
2016-10-03 22:44:07 -0700

    first commit

---
+++ models/Posts.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+PostSchema.methods.upvote = function(post) {
+  this.upvotes += 1;
+  this.save(post);
+};
+
+mongoose.model('Post', PostSchema);
