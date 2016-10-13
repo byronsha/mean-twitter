dc92a2c801ce8ca75e6744359f3df7466befa42c
Byron Sha <byronysha@gmail.com>
2016-10-13 01:36:00 -0700

    Create Heroes Service

--- public/javascripts/live-league-games/live-league-games-controller.js
+++ public/javascripts/live-league-games/live-league-games-controller.js
@@ -1,5 +1,7 @@
 angular.module('flapperNews')
-.controller('LiveLeagueGamesController', ['$scope', 'liveLeagueGames', 'auth', function($scope, liveLeagueGames, auth) {
+.controller('LiveLeagueGamesController', ['$scope', 'liveLeagueGames', 'heroes', 'auth', function($scope, liveLeagueGames, heroes, auth) {
   $scope.liveLeagueGames = liveLeagueGames.games;
+  $scope.heroes = heroes;
   $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.imageExt = '_sb.png';
 }]);
